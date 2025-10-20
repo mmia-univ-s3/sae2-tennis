aa99915145cb3a5362dc7f94277216e1220a1375
mmia-univ <mia.murat@etu.univ-orleans.fr>
2025-10-20 15:32:09 +0200

    Ajout admin + retrait Open Masc 86

--- appli/app.py
+++ appli/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap5 import Bootstrap
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -8,5 +9,7 @@ app.config.from_object('config')
 
 db = SQLAlchemy()
 Bootstrap(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "connexion"
 
 db.init_app(app)
