cfeccd2daa5212571fcd9c7f7485e2453eb1990d
Mia <mia.murat@etu.univ-orleans.fr>
2025-11-03 12:18:29 +0100

    Correction d'un bug avec Affronter

--- appli/models/affronter.py
+++ appli/models/affronter.py
@@ -10,7 +10,7 @@ class Affronter(db.Model):
                                                   onupdate="CASCADE"), primary_key=True)
     _id_equipe: int = db.Column("idE", db.Integer,
                                 db.ForeignKey("EQUIPE.idE"), primary_key=True)
-    adversaire: str = db.Column("nomAdv", db.Text, primary_key=True)
+    adversaire: str = db.Column("nomAdv", db.String(100), primary_key=True)
     resultat: str = db.Column("resultat", db.Text)
     score: int = db.Column("score", db.Text)
     stade: str = db.Column("stade", db.Text)
