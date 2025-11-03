e2005d22e09c174db961da813250af566a623151
mmia-univ <mia.murat@etu.univ-orleans.fr>
2025-11-03 09:04:48 +0100

    Ajout de la gestion des utilisateurs

--- appli/forms.py
+++ appli/forms.py
@@ -1,8 +1,12 @@
+from hashlib import sha256
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, HiddenField
 # from wtforms.fields.numeric import FloatField
 from wtforms.fields.simple import PasswordField
 from wtforms.validators import DataRequired
+
+from appli.models.utilisateur import Utilisateur
 
 
 # from wtforms.validators import DataRequired
@@ -19,10 +23,10 @@ class LoginForm(FlaskForm):
 
     def get_authenticated_user(self):
         pass
-        # user = User.query.get(self.login.data)
-        # if user is None:
-        #     return
-        # m = sha256()
-        # m.update(self.password.data.encode())
-        # password = m.hexdigest()
-        # return user if password == user.password else None
+        user = Utilisateur.query.get(self.login.data)
+        if user is None:
+            return None
+        m = sha256()
+        m.update(self.password.data.encode())
+        password = m.hexdigest()
+        return user if password == user.password else None
