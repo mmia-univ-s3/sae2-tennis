c298d0f0b539f5fbb5ea2329e779a5f20b887feb
mmia-univ <mia.murat@etu.univ-orleans.fr>
2025-11-03 09:24:00 +0100

    Correction d'une ligne trop longue

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql://murat:murat@servinfo-maria/DBmurat')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+                                         'mysql://murat:murat@servinfo-maria/DBmurat')
 BOOTSTRAP_SERVE_LOCAL = True
 
 SECRET_KEY = "1w{euOn#)\x0c<EA0RO7O>8|w0"
