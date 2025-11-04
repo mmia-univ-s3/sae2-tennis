b86e633368d69aea22360c2319eb7e0b9e0b9e2b
mmia-univ <mia.murat@etu.univ-orleans.fr>
2025-11-04 11:29:15 +0100

    Ajout de la page Documents administratifs

--- appli/views.py
+++ appli/views.py
@@ -42,9 +42,22 @@ def management():
 def articles():
     return render_template('articles.html', title="Articles du club - Club")
 
-@app.route('/club/documents/')
+@app.route('/club/documents/', methods=('GET', 'POST'))
 def documents():
-    return render_template('documents.html', title="Documents administratifs - Club")
+    form = PageForm()
+    article = Article.query.filter(Article.titre == "_documents" and
+                                   Article.type_article == "pages").first()
+    if article is None:
+        article = Article("_documents", "", datetime.date.today(), "pages")
+        db.session.add(article)
+        db.session.commit()
+    if current_user.is_authenticated:
+        if form.validate_on_submit():
+            article.contenu = form.editor.data
+            article.date = datetime.date.today()
+            db.session.commit()
+    return render_template('documents.html', title="Documents administratifs - Club",
+                           contenu=article.contenu, form=form)
 
 @app.route('/competitions/')
 def competitions():
