4a70dd005eb0267ab073dd955402196d5f775b0c
Guedon Anaelle <o22402381@or-iut-i002-f20.iut45.univ-orleans.fr>
2025-11-04 14:04:00 +0100

    correction pylint

--- appli/views.py
+++ appli/views.py
@@ -55,16 +55,19 @@ def formation():
 
 @app.route('/formation/tarifications/')
 def tarifications():
-    list_cate_tennis = CategorieTarif.query.filter(CategorieTarif._id_parent == None, CategorieTarif.sport == "tennis").all()
+    list_cate_tennis = CategorieTarif.query.filter(CategorieTarif._id_parent is None,
+                                                   CategorieTarif.sport == "tennis").all()
     list_cate_rese_tennis = filter(lambda x: len(x.enfants) > 0 and \
                             len(x.enfants[0].tarifs.all()) > 0 and \
                             len(x.enfants[0].tarifs[0].reservations.all()) > 0, list_cate_tennis)
     list_cate_redu_tennis = filter(lambda x: len(x.enfants) > 0 and \
                             len(x.enfants[0].tarifs.all()) > 0 and \
                             len(x.enfants[0].tarifs[0].reductions.all()) > 0, list_cate_tennis)
-    list_cate_padel = CategorieTarif.query.filter(CategorieTarif._id_parent == None,
+    list_cate_padel = CategorieTarif.query.filter(CategorieTarif._id_parent is None,
                                                   CategorieTarif.sport == "padel").all()
-    return render_template('tarifications.html', title="Tarifications - Formation", cate_rese=list_cate_rese_tennis, cate_redu=list_cate_redu_tennis, cate_padel=list_cate_padel)
+    return render_template('tarifications.html', title="Tarifications - Formation",
+                           cate_rese=list_cate_rese_tennis, cate_redu=list_cate_redu_tennis,
+                           cate_padel=list_cate_padel)
 
 @app.route('/formation/ecole-de-tennis/')
 def ecole():
