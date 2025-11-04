7a5eb800bbbcb78cd5b8be7646076ffc7008e730
Maiawella Frelat <maiawella.feve@etu.univ-orleans.fr>
2025-11-04 09:04:20 +0100

    Modifications des fichiers selon pylint

--- appli/views.py
+++ appli/views.py
@@ -77,7 +77,7 @@ def partenaire_delete(id_p: int):
         db.session.commit()
         os.remove('./static/' + part.logo)
         return redirect(url_for("partenaires"))
-    return render_template("partenaires_delete.html", form=form, 
+    return render_template("partenaires_delete.html", form=form,
                            title="Suppression d'un partenaire", parte = part)
 
 @app.route('/partenaire/ajout/', methods=("GET", "POST",))
