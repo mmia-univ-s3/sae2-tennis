e2f77617827a1faed60b33157ff8d262b8d045b3
Maïawella <maiawella.feve@etu.univ-orleans.fr>
2025-11-04 11:00:37 +0100

    Apply suggestions from code review
    Co-authored-by: Mia <mia.murat@etu.univ-orleans.fr>

--- appli/forms.py
+++ appli/forms.py
@@ -41,7 +41,7 @@ class ConfirmForm(FlaskForm):
 class PartenairesCreateForm(FlaskForm):
     nom = StringField('Nom du partenaire', validators=[DataRequired()])
     logo = FileField('Logo du partenaire (JPG ou PNG uniquement)',
-                     validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Merci de n'envoyer que des fichiers JPG ou PNG.')])
+                     validators=[FileRequired(), FileAllowed(['jpg', 'png'], "Merci de n'envoyer que des fichiers JPG ou PNG.")])
     next = HiddenField()
 
     def confirm(self, filename):
