c883058676324dd3d0448d1c3c9eb27475b20c65
Guedon Anaelle <o22402381@or-iut-i002-f20.iut45.univ-orleans.fr>
2025-11-04 14:09:20 +0100

    corrections pylint

--- appli/views.py
+++ appli/views.py
@@ -55,6 +55,7 @@ def formation():
 
 @app.route('/formation/tarifications/')
 def tarifications():
+    # pylint: disable=protected-access
     list_cate_tennis = CategorieTarif.query.filter(CategorieTarif._id_parent is None,
                                                    CategorieTarif.sport == "tennis").all()
     list_cate_rese_tennis = filter(lambda x: len(x.enfants) > 0 and \
@@ -63,7 +64,8 @@ def tarifications():
     list_cate_redu_tennis = filter(lambda x: len(x.enfants) > 0 and \
                             len(x.enfants[0].tarifs.all()) > 0 and \
                             len(x.enfants[0].tarifs[0].reductions.all()) > 0, list_cate_tennis)
-    list_cate_padel = CategorieTarif.query.filter(CategorieTarif._id_parent is None,
+    # pylint: disable=protected-access
+    list_cate_padel = CategorieTarif.query.filter(CategorieTarif._id_parent is None
                                                   CategorieTarif.sport == "padel").all()
     return render_template('tarifications.html', title="Tarifications - Formation",
                            cate_rese=list_cate_rese_tennis, cate_redu=list_cate_redu_tennis,
