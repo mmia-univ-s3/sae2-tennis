1d32a6a5acd7d062c1fa03c5c2960b24cb1d7e3b
Maiawella Frelat <maiawella.feve@etu.univ-orleans.fr>
2025-11-05 15:43:58 +0100

    Implémentation de l'option de modification sur Tarification

--- appli/forms.py
+++ appli/forms.py
@@ -75,7 +75,7 @@ class CategorieForm(FlaskForm):
     intituleCat = StringField("L'intitulé de la catégorie", validators=[DataRequired()])
     sport = SelectField("Sport", validators=[DataRequired()],
                         choices=[("tennis", "Tennis"), ("padel", "Padel")])
-
+    
 class PageForm(FlaskForm):
     editor = StringField()
 
