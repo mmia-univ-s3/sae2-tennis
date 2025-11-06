0fe152bb4ff02cfa9a58bf0db40a2d6320446765
Guedon Anaelle <o22402381@or-iut-i002-f20.iut45.univ-orleans.fr>
2025-11-06 09:23:53 +0100

    pages de modifications dans contact

--- appli/views.py
+++ appli/views.py
@@ -326,7 +326,82 @@ def partenaire_create():
 
 @app.route('/contacts/')
 def contacts():
-    return render_template('contacts.html', title="Contacts")
+    adresse = Article.query.filter(Article.titre == "_adresse" and
+                                   Article.type_article == "pages").first()
+    if adresse is None:
+        adresse = Article("_adresse", "", datetime.date.today(), "pages")
+        db.session.add(adresse)
+        db.session.commit()
+    tel = Article.query.filter(Article.titre == "_tel" and
+                                   Article.type_article == "pages").first()
+    if tel is None:
+        tel = Article("_tel", "", datetime.date.today(), "pages")
+        db.session.add(tel)
+        db.session.commit()
+    mail = Article.query.filter(Article.titre == "_mail" and
+                                   Article.type_article == "pages").first()
+    if mail is None:
+        mail = Article("_mail", "", datetime.date.today(), "pages")
+        db.session.add(mail)
+        db.session.commit()
+    return render_template('contacts.html', title="Contacts", adresse=adresse.contenu, tel=tel.contenu, mail=mail.contenu)
+
+@app.route('/contacts/modif_adresse/', methods=("GET", "POST",))
+@login_required
+def contacts_modif_adresse():
+    form = PageForm()
+    article = Article.query.filter(Article.titre == "_adresse" and
+                                   Article.type_article == "pages").first()
+    if article is None:
+        article = Article("_adresse", "", datetime.date.today(), "pages")
+        db.session.add(article)
+        db.session.commit()
+    if current_user.is_authenticated:
+        if form.validate_on_submit():
+            article.contenu = form.editor.data
+            article.date = datetime.date.today()
+            db.session.commit()
+            return redirect(url_for('contacts'))
+    return render_template('contacts_modif_adresse.html', title="Contacts",
+                           form=form, contenu=article.contenu)
+    
+@app.route('/contacts/modif_tel/', methods=("GET", "POST",))
+@login_required
+def contacts_modif_tel():
+    form = PageForm()
+    article = Article.query.filter(Article.titre == "_tel" and
+                                   Article.type_article == "pages").first()
+    if article is None:
+        article = Article("_tel", "", datetime.date.today(), "pages")
+        db.session.add(article)
+        db.session.commit()
+    if current_user.is_authenticated:
+        if form.validate_on_submit():
+            article.contenu = form.editor.data
+            article.date = datetime.date.today()
+            db.session.commit()
+            return redirect(url_for('contacts'))
+    return render_template('contacts_modif_tel.html', title="Contacts",
+                           form=form, contenu=article.contenu)
+    
+@app.route('/contacts/modif_mail/', methods=("GET", "POST",))
+@login_required
+def contacts_modif_mail():
+    form = PageForm()
+    article = Article.query.filter(Article.titre == "_mail" and
+                                   Article.type_article == "pages").first()
+    if article is None:
+        article = Article("_mail", "", datetime.date.today(), "pages")
+        db.session.add(article)
+        db.session.commit()
+    if current_user.is_authenticated:
+        if form.validate_on_submit():
+            article.contenu = form.editor.data
+            article.date = datetime.date.today()
+            db.session.commit()
+            return redirect(url_for('contacts'))
+    return render_template('contacts_modif_mail.html', title="Contacts",
+                           form=form, contenu=article.contenu)
 
 @app.route('/autre-sports/', methods=('GET', 'POST'))
 def autre():
