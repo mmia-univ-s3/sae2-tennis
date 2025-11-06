387cb04f905e39ce729e0872217d57a881845dbc
Naick1207 <naick.cher@etu.univ-orleans.fr>
2025-11-06 09:56:16 +0100

    Refonte du SQLAlchemy

--- analyse/mld.md
+++ analyse/mld.md
@@ -11,7 +11,7 @@ DIVISION[<u>idDiv</u>, intituleDiv]
 CHAMP_EQUIPE[<u>idCha</u>, dateCha, titreCha, categorieSport, serie]  
 EQUIPE[<u>idE</u>, nomE, categorieE, #idDiv, rangDiv]  
 PARTICIPER[<u>#idCha, #idE</u>, rang, poule]  
-AFFRONTER[<u>#idCha, #idE, nomAdv, dateMatch</u>, resultat, score, estDomicile]
+AFFRONTER[<u>#idCha, #idE, nomAdv, dateMatch</u>, resultat, score, estDomicile]  
 
 CATEGORIE_TARIF[<u>idCat</u>, sport, intituleCat, #idCatParent]  
 TARIF[<u>idT</u>, intituleT, #idCat]  
--- appli/models/reduction.py
+++ appli/models/reduction.py
@@ -7,15 +7,15 @@ class Reduction(db.Model):
                                db.ForeignKey("TARIF.idT", ondelete="CASCADE",onupdate="CASCADE"),
                                primary_key=True)
     taux: str = db.Column("taux", db.Text)
-    cumulable: bool = db.Column("estCumulable", db.Boolean)
+    licence: bool = db.Column("surLicence", db.Boolean)
 
     tarif = db.relationship("Tarif", backref=db.backref("reductions",
                             lazy="dynamic", cascade="all, delete-orphan"))
 
-    def __init__(self, id_tarif: int, taux: str, cumulable: bool):
+    def __init__(self, id_tarif: int, taux: str, licence: bool):
         self._id_tarif = id_tarif
         self.taux = taux
-        self.cumulable = cumulable
+        self.licence = licence
 
     def __str__(self):
         return f"<Reduction({self._id_tarif}) {self.tarif.intitule} {self.taux}>"
