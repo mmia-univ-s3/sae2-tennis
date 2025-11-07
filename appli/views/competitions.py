17abdac67a69e76b63b2462e81e3ef65a78479a1
Naick1207 <naick.cher@etu.univ-orleans.fr>
2025-11-07 14:17:07 +0100

    Ajout et Suppresion des championnats et participants des championnats indiv

--- appli/views/competitions.py
+++ appli/views/competitions.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from flask_login import login_required
 
-from appli.app import app
-from appli.models import ChampionnatIndividuel, ChampionnatEquipe, Affronter
+from appli.app import app, db
+from appli.models import ChampionnatIndividuel, ChampionnatEquipe, Affronter, Joueur, Classer
+from appli.forms import FormChampionnatEquipe, FormChampionnatIndividuel, FormClasser
 
 
 @app.route('/competitions/calendrier/')
@@ -10,6 +12,59 @@ def calendrier():
     list_comp_indiv = ChampionnatIndividuel.query.order_by(ChampionnatIndividuel.date_championnat.desc()).all()
     list_comp_equipe = ChampionnatEquipe.query.order_by(ChampionnatEquipe.date_championnat.desc()).all()
     return render_template('calendrier.html', title="Calendrier - Compétitions", comp_indiv=list_comp_indiv, comp_equipe=list_comp_equipe)
+
+@app.route('/competitions/tournoi/<type_tournoi>/<int:idC>/delete/', methods=('GET', 'POST'))
+@login_required
+def tournoi_delete(type_tournoi: str, idC: int):
+    """Permet de supprimer un tournoi de la base de données
+
+    Args:
+        type_tournoi (str): Indique si le tournoi est "individuel" ou en "equipe"
+        idC (int): L'identifiant du tournoi dans la base de données
+    """
+    if type_tournoi == "individuel":
+        championnat = ChampionnatIndividuel.query.get(idC)
+        form = FormChampionnatIndividuel(titre=championnat.titre,
+                                         date_championnat=championnat.date_championnat,
+                                         categorie=championnat.categorie, serie=championnat.serie,
+                                         niveau=championnat.niveau)
+    else:
+        championnat = ChampionnatEquipe.query.get(idC)
+        form = FormChampionnatEquipe(titre=championnat.titre,
+                                     date_championnat=championnat.date_championnat,
+                                     categorie=championnat.categorie, serie=championnat.serie)
+    if form.validate_on_submit():
+        db.session.delete(championnat)
+        db.session.commit()
+        return redirect(url_for("calendrier"))
+    return render_template('tournoi_delete.html', title="Supprimer un tournoi", form=form,
+                           type_tournoi=type_tournoi, championnat=championnat)
+
+@app.route('/competitions/tournoi/<type_tournoi>/add', methods=('GET', 'POST'))
+@login_required
+def tournoi_add(type_tournoi: str):
+    """Permet d'ajouter un tournoi dans la base de données
+
+    Args:
+        type_tournoi (str): Indique si le tournoi est "individuel" ou en "equipe"
+    """
+    if type_tournoi == "individuel":
+        form = FormChampionnatIndividuel()
+        championnat = ChampionnatIndividuel(titre=form.titre.data,
+                                            date_championnat=form.date_championnat.data,
+                                            categorie=form.categorie.data, serie=form.serie.data,
+                                            niveau=form.niveau.data)
+    else:
+        form = FormChampionnatEquipe()
+        championnat = ChampionnatEquipe(titre=form.titre.data,
+                                        date_championnat=form.date_championnat.data,
+                                        categorie=form.categorie.data, serie=form.serie.data)
+    if form.validate_on_submit():
+        db.session.add(championnat)
+        db.session.commit()
+        return redirect(url_for("calendrier"))
+    return render_template('tournoi_add.html', title="Ajouter un tournoi", form=form,
+                           type_tournoi=type_tournoi, championnat=championnat)
 
 @app.route('/competitions/palmares/')
 def palmares():
@@ -45,6 +100,56 @@ def tournoi(type_tournoi: str, idC: int):
             liste_dates[id_equipe].sort()
     return render_template('tournoi.html', title="Tournoi - Competitions", championnat=champ,
                            type_champ=type_tournoi, matchs=donnees, dates=liste_dates)
+    
+@app.route('/competitions/tournoi/individuel/<int:idC>/<int:idJ>/delete/', methods=('GET', 'POST'))
+@login_required
+def participant_indiv_delete(idC: int, idJ: int):
+    """Permet de supprimer un participant d'un tournoi individuel
+
+    Args:
+        idC (int): L'identifiant du tournoi dans la base de données
+        idJ (int): L'identifiant d'un joueur dans la base de données
+    """
+    joueur = Joueur.query.get(idJ)
+    championnat = ChampionnatIndividuel.query.get(idC)
+    classer = Classer.query.get((idC, idJ))
+    lesJoueurs = Joueur.query.all()
+    choix = []
+    for donnee in lesJoueurs:
+        choix.append((donnee.id, f"{donnee.prenom} {donnee.nom}"))
+    form = FormClasser(joueur=idJ, rang=classer.rang)
+    form.joueur.choices = choix
+    if form.validate_on_submit():
+        db.session.delete(classer)
+        db.session.commit()
+        return redirect(url_for("tournoi", type_tournoi="individuel", idC=idC))
+    return render_template('participant_indiv_delete.html', title="Supprimer un participant", form=form,
+                           championnat=championnat, joueur=joueur)
+
+@app.route('/competitions/tournoi/individuel/<int:idC>/add/', methods=('GET', 'POST'))
+@login_required
+def participant_indiv_add(idC: int):
+    """Permet de supprimer un participant d'un tournoi individuel
+
+    Args:
+        idC (int): L'identifiant du tournoi dans la base de données
+    """
+    championnat = ChampionnatIndividuel.query.get(idC)
+    form = FormClasser()
+    lesJoueurs = Joueur.query.all()
+    choix = []
+    for donnee in lesJoueurs:
+        choix.append((donnee.id, f"{donnee.prenom} {donnee.nom}"))
+    form = FormClasser()
+    form.joueur.choices = choix
+    if form.validate_on_submit():
+        classer = Classer(id_championnat=idC, id_j=form.joueur.data, rang=form.rang.data)
+        db.session.add(classer)
+        db.session.commit()
+        return redirect(url_for("tournoi", type_tournoi="individuel", idC=idC))
+    return render_template('participant_indiv_add.html', title="Supprimer un participant", form=form,
+                           championnat=championnat)
+
 
 @app.route('/competitions/tournois-internes/')
 def internes():
