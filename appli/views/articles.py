855e884fccc51d1cfa4c47b5aae45d9754b5c2ef
Guedon Anaelle <o22402381@or-iut-i002-f20.iut45.univ-orleans.fr>
2026-02-10 09:42:31 +0100

    Ajout d'un bouton pour supprimer une image + description d'une image + sélection de la largeur d'une image

--- appli/views/articles.py
+++ appli/views/articles.py
@@ -30,10 +30,10 @@ def article_view(id_article):
                 filename = form.filename()
                 image = form.image.data
                 image.save(os.path.join("appli", "static", "upload", filename))
-                if ancienne_image  != "" and ancienne_image is not None and os.path.exists(
-                        os.path.join("appli", "static", "upload", ancienne_image)):
-                    os.remove(os.path.join("appli", "static", "upload", ancienne_image))
-            form.update_article(article, filename)
+                if ancienne_image is not None and ancienne_image.nom_fichier != "" and os.path.exists(
+                        os.path.join("appli", "static", "upload", ancienne_image.nom_fichier)):
+                    os.remove(os.path.join("appli", "static", "upload", ancienne_image.nom_fichier))
+            form.update_article(article, filename, form.largeur.data, form.description.data)
 
 
     return render_template("article_view.html", title=article.titre, article=article, form=form,
@@ -52,7 +52,7 @@ def article_create():
                 filename = form.filename()
                 image = form.image.data
                 image.save(os.path.join("appli", "static", "upload", filename))
-            article = form.creation_article(filename)
+            article = form.creation_article(filename, form.largeur.data, form.description.data)
             return redirect(form.next.data or url_for("article_view", id_article=article.id))
     return render_template("article_add.html", title="Ajout d'un article", form=form)
 
@@ -72,3 +72,18 @@ def article_delete(id_article):
         return redirect(url_for("articles"))
     return render_template("article_delete.html", form=form,
                            title="Suppression d'un article", article=article)
+
+
+@app.route('/club/articles/<id_article>/delete/image/', methods=('GET', 'POST'))
+@required_permission_lvl("ecrivain")
+def article_delete_image(id_article):
+    """Page de suppression d'une image d'un article"""
+    form = FormConfirm()
+    article = Article.query.get(id_article)
+    if form.validate_on_submit():
+        if article.image != "" and article.image is not None:
+            db.session.delete(article.image)
+            db.session.commit()
+            return redirect(url_for("articles"))
+    return render_template("article_delete_image.html", form=form,
+                           title="Suppression d'une image d'un article", article=article)
