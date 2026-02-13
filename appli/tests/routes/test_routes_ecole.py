79613e4b318bf8c9e7cc21ee23709a8fea0dec36
mmia-univ <mia.murat@etu.univ-orleans.fr>
2026-02-13 09:46:59 +0100

    Corrections sur les tests de histoire

--- appli/tests/routes/test_routes_ecole.py
+++ appli/tests/routes/test_routes_ecole.py
@@ -2,4 +2,4 @@
 
 def test_ecole(client):
     response = client.get('/formation/ecole-de-tennis/', follow_redirects=True)
-    assert b"cole de Tennis" in response.data
+    assert b"Ecole de tennis 1" in response.data
