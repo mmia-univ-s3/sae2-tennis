f6f0c867f249ca6816916d204289abc5c928f8f0
mmia-univ <mia.murat@etu.univ-orleans.fr>
2026-02-13 09:39:55 +0100

    Corrections sur les tests de autre

--- appli/tests/routes/test_routes_autre.py
+++ appli/tests/routes/test_routes_autre.py
@@ -2,4 +2,4 @@
 
 def test_autres(client):
     response = client.get('/autre-sports/', follow_redirects=True)
-    assert b"Autres sports sur le stade" in response.data
+    assert b"Aquaponey" in response.data
